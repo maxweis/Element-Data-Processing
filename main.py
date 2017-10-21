c1087146fbd4f420ce1baa2acde402d45bb97238
Max Weis <maxrobweis@gmail.com>
2017-10-21 15:36:33 -0500

    cleanup

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-#!/bin/python
 from openpyxl import Workbook
 from openpyxl.compat import range
 from openpyxl.utils import get_column_letter
 import os
 
+#TODO
+# add total
+# fix ordering 
+
 #sorts through data in given format from files in current directory with the
 #defined file extension, searches for defined element isotopes
 DATA_FILE_EXTENSION = ".plt"
-ELEMENT = "pu"
+ELEMENT = "am"
 OUTPUT_FILE = ELEMENT + " raw data.xlsx"
 
 #sort through a list of files, return files with the given extension
 def sort_file_extensions(files, extension):
     return [i for i in files if i[-len(extension):] == extension]
+
+# def file_numeric_sort(files):
+    # for filename in files:
 
 def get_file_lines(filename):
     with open(filename, "r") as open_file: #open file to read and close when donw
@@ -23,14 +29,14 @@ def row1_data(file_lines):
     return file_lines[0].split()[3][:-1]
 
 def col_b_data(file_lines):
-    return ["total mass of unit", "burnup(days)"] + file_lines[5].split() #e replacement because notation
+    return ["total mass of unit", "burnup(days)"] + file_lines[5].split()
 
 #get element data for columns
 def get_col_element_data(file_lines, element):
     output = []
     for line in file_lines[6:]:
         if line.split()[0][:len(element)] == element:
-            output = output + [line.strip().split()] #e replacement because notation
+            output = output + [line.strip().split()]
     output[0] = [row1_data(file_lines)] + output[0]
 
     return output
@@ -41,7 +47,7 @@ def main():
     sheet1 = wb.active
     sheet1.title = ELEMENT + " raw data"
 
-    directory_files = os.listdir("./") #get files in current directory
+    directory_files = sorted(os.listdir("./")) #get files in current directory
 
     current_column = 0
 
